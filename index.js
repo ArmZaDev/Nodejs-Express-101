97046cb03e3856babf5257f9b22401cf2cd3c60f
ArmZaDev <nopparat.p62@rsu.ac.th>
2023-03-23 17:33:39 +0700

    Modules url

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const http = require('http');
-const Fs = require('fs');
+const fs = require('fs');
+const url = require('url');
 
-const indexPage = Fs.readFileSync(`${__dirname}/templates/index.html`)
-const productPage = Fs.readFileSync(`${__dirname}/templates/product1.html`);
+const indexPage = fs.readFileSync(`${__dirname}/templates/index.html`, 'utf-8');
+const productPage1 = fs.readFileSync(`${__dirname}/templates/product1.html`, 'utf-8');
+const productPage2 = fs.readFileSync(`${__dirname}/templates/product2.html`, 'utf-8');
+const productPage3 = fs.readFileSync(`${__dirname}/templates/product3.html`, 'utf-8');
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
-    console.log("url = ", pathName)
 
+    const {pathName, query} = url.parse(req.url, true);
     if(pathName === "/" || pathName === "/home"){
         res.end(indexPage);
     }
     else if(pathName === "/product"){
-        res.end(productPage);
+        //ข้อมูลสินค้าชิ้นที่ 1
+        if(query.id === "1"){
+            res.end(productPage1);
+        }
+        else if(query.id === "2"){
+            res.end(productPage2);
+        }
+        else if(query.id === "3"){
+            res.end(productPage3);
+        }
+        else{
+            res.writeHead(404);
+            res.end("<h1>Not Found</h1>");
+        }
     }
     else{
         res.writeHead(404);
@@ -20,5 +35,5 @@ const server = http.createServer((req, res) => {
     }
 });
 
-server.listen(8080, 'localhost',() => 
-    console.log("Server Running at port 8080"));
+server.listen(3000, 'localhost',() => 
+    console.log("Server Running at port 3000"));
