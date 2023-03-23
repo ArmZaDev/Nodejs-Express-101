3621519106fd1ecb2120767c27b04730e690180f
ArmZaDev <arztv001@gmail.com>
2023-03-23 21:19:01 +0700

    Module path

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express(); //object express
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello Express.js</h1>")
+    res.status(200);
+    res.type("text/html");
+    res.sendFile(path.join(__dirname,"templates/index.html"))
+})
+
+app.get("/product", (req, res) => {
+    res.sendFile(path.join(__dirname,"templates/product1.html"))
 })
 
 app.listen(8080, () =>{
