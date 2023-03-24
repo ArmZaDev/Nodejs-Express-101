f4dfac53412722e60722d2e6764e85966d7e842a
ArmZaDev <nopparat.p62@rsu.ac.th>
2023-03-24 20:05:38 +0700

    View & Template (EJS)

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const path = require('path');
+const router = require('./routes/router.js');
 const app = express(); //object express
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engin', 'ejs-lint');
+
+app.use(router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(8080, () =>{
--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
+
+router.get('/', (req, res) => {
+    const products = [
+        {name:"โน๊ตบุ๊ค", price:25500, image:"images/products/product1.png"},
+        {name:"เสื้อผ้า", price:2000, image:"images/products/product2.png"},
+        {name:"หูฟัง", price:800, image:"images/products/product3.png"},
+    ]
+    res.render('index.ejs',{products:products});
+})
 
 module.exports = router;
