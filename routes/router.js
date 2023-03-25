3911de77c6b7f1dae110ba916975f46a41f102c1
ArmZaDev <arztv001@gmail.com>
2023-03-25 22:22:30 +0700

    Insert

--- routes/router.js
+++ routes/router.js
@@ -8,17 +8,15 @@ const storage = multer.diskStorage({
         cb(null, './public/images/products');
     },
     filename:function(req, file, cb){
-        cb(null, Date.now() + ".jpg")
+        cb(null, Date.now() + ".jpg");
     }
 })
 
 router.get('/', (req, res) => {
-    const products = [
-        {name:"โน๊ตบุ๊ค", price:25500, image:"images/products/product1.png"},
-        {name:"เสื้อผ้า", price:2000, image:"images/products/product2.png"},
-        {name:"หูฟัง", price:800, image:"images/products/product3.png"},
-    ]
-    res.render('index',{products:products});
+    Product.find().exec((err, doc) => {
+        res.render('index',{products:doc});
+    })
+    
 })
 
 router.get('/addForm', (req, res) => {
@@ -26,7 +24,9 @@ router.get('/addForm', (req, res) => {
 })
 
 router.get('/manage', (req, res) => {
-    res.render('manage');
+    Product.find().exec((err, doc) => {
+        res.render('manage',{products:doc});
+    })
 })
 
 const upload = multer({
@@ -37,10 +37,10 @@ router.post('/insert', upload.single("image"), (req, res) => {
     let data = new Product({
         name:req.body.name,
         price:req.body.price,
-        image:req.file.fieldname,
+        image:req.file.filename,
         description:req.body.description
     })
-    Product.saveProduct(data, function(err){
+    Product.saveProduct(data, (err) => {
         if(err) console.log(err);
         res.redirect('/');
     })
